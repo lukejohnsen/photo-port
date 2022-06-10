a28b8812c7916b930ae72066f5d7f2baef01ab0a
lukejohnsen <johnsen.luke@gmail.com>
2022-06-10 16:04:08 -0700

    making project a SPA, adding conditional rendering

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -47,15 +47,20 @@ function ContactForm() {
             <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name' defaultValue={name} onChange={handleChange} />
+                    <input type='text' name='name' defaultValue={name} onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor='email'>Email address:</label>
-                    <input type='email' name='email' defaultValue={email} onChange={handleChange} />
+                    <input type='email' name='email' defaultValue={email} onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea name='message' rows='5' defaultValue={message} onChange={handleChange} />
+                    <textarea name='message' rows='5' defaultValue={message} onBlur={handleChange} />
+                    {errorMessage && (
+                        <div>
+                            <p className='error-text'>{errorMessage}</p>
+                        </div>
+                    )}
                 </div>
                 <button type='submit'>Submit</button>
             </form>
