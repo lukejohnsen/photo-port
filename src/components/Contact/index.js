d7c517513276efac47efb1341eda4d748cd6ee81
lukejohnsen <johnsen.luke@gmail.com>
2022-06-10 16:24:43 -0700

    creating baseline and snapshot tests for Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -43,7 +43,7 @@ function ContactForm() {
 
     return (
         <section>
-            <h1>Contact me</h1>
+            <h1 data-testid='h1tag'>Contact me</h1>
             <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='name'>Name:</label>
@@ -62,7 +62,7 @@ function ContactForm() {
                         </div>
                     )}
                 </div>
-                <button type='submit'>Submit</button>
+                <button type='submit' data-testid='button'>Submit</button>
             </form>
         </section>
     )
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -12,6 +12,8 @@ const categories = [
 
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -22,6 +24,8 @@ describe('Nav component', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
             />);
     });
 
@@ -31,6 +35,8 @@ describe('Nav component', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
             />);;
         expect(asFragment()).toMatchSnapshot();
     });
@@ -43,6 +49,8 @@ describe('emoji is visible', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
             />);
 
         // Assert
@@ -57,6 +65,8 @@ describe('links are visible', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
             />);;
 
         // Assert
